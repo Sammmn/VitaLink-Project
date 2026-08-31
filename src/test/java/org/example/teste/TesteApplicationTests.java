7dfdb6c11f07bacd671a1164f035fa015dc943c4
Sammmn <Samuel.marques414@gmail.com>
2026-08-31 18:02:03 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/org/example/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
