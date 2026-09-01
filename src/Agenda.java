6e85236aebe5662e247c10afe4fe8486938c2032
AleSandro462 <aplfilho08@gmail.com>
2026-09-01 00:57:43 -0300

    Adicionando classes
    Adicionei as classes iniciais que vão ser usadas. Depois é só organizá-las nas pastas.

---
+++ src/Agenda.java
@@ -0,0 +1,21 @@
+public class Agenda {
+    //private Cliente cliente;
+    //private Medico medico;
+    //private Interacao interacao;
+
+    public void agendarOperacao(Cliente cliente, Medico medico, Interacao interacao) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Interacao.java
@@ -0,0 +1,5 @@
+public class Interacao {
+    String nomeDaInteracao;
+    String localDaInteracao;     // a consulta vai ser na sala 204 por exemplo
+    String data;
+}
