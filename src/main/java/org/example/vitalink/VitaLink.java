aaca0444967446f7a1aa4cb6e5b914e015b7950a
Sammmn <Samuel.marques414@gmail.com>
2026-09-05 22:00:19 -0300

    Renomeando a classe main

--- src/main/java/org/example/vitalink/VitaLink.java
+++ src/main/java/org/example/vitalink/VitaLink.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TesteApplication {
+public class VitaLink {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(TesteApplication.class, args);
+        SpringApplication.run(org.example.teste.VitaLink.class, args);
     }
 
 }
